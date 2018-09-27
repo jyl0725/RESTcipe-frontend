135c42f8b1b014e6bb90fc44ac1b53537bfe36d9
github username <github email address>
2018-09-27 10:56:19 -0400

    basic fetch

--- restcipe/src/App.js
+++ restcipe/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch()
+  }
+
   render() {
     return (
       <div className="App">
